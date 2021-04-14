d7f9beb816e211a525377fad9c432c1b1155ac2c
Sean Jones <seanjones2848@gmail.com>
2021-04-14 13:14:57 -0700

    adding in a sprite

--- rust_game/src/main.rs
+++ rust_game/src/main.rs
@@ -1,27 +1,42 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::render::WindowCanvas;
+use sdl2::render::{WindowCanvas, Texture};
+use sdl2::rect::{Point, Rect};
+use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas, color: Color) {
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    position: Point,
+    sprite: Rect,
+) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
+    let (width, height) = canvas.output_size()?;
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+    canvas.copy(texture, sprite, screen_rect)?;
     canvas.present();
+    Ok(())
 }
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
     let window = video_subsystem.window("rust_game", 800, 600)
         .position_centered()
         .build()
         .expect("couldn't initialize video subsystem");
     let mut canvas = window.into_canvas().build()
         .expect("could not make canvas");
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+    let position = Point::new(0, 0);
+    let sprite = Rect::new(0, 0, 26, 36);
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
@@ -38,7 +53,7 @@ fn main() -> Result<(), String> {
         // update
         i = (i + 1) % 255;
         // render
-        render(&mut canvas, Color::RGB(i, 64, 255 - i));
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite)?;
         // time management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
